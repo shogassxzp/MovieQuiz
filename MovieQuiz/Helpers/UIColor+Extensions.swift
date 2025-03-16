0f0abb0d0d0a3216e35f52382374a574910b15be
rssxzp <rssxzp@icloud.com>
2025-03-16 14:06:11 +0500

    added launchscreen

---
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIColor {  static var YPGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+    static var YPRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var YPBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var YPBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var YPGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var YPWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+}
