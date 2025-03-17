2bd3a92c65c9ffdc242d9bef58cde7bfa9c84f5c
rssxzp <rssxzp@icloud.com>
2025-03-18 02:37:30 +0500

    version beta 0.1

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,17 +1,27 @@
 import UIKit
+
 struct QuizQuestion {
-let image: String
-let text: String
-let correctAnswer: Bool
-}
+    let image: String
+    let text: String
+    let correctAnswer: Bool }
+
 struct QuizStepViewModel {
-let image: UIImage
-let question: String
-let questionNumber: String
+    let image: UIImage
+    let question: String
+    let questionNumber: String }
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
 }
+
 let currentQuestion = questions[currentQuestionIndex]
+
 private var currentQuestionIndex = 0
+
 private var correctAnswers = 0
+
 private let questions: [QuizQuestion] = [
     QuizQuestion(image:"The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
     QuizQuestion(image:"The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
@@ -23,6 +33,7 @@ private let questions: [QuizQuestion] = [
     QuizQuestion(image:"The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
     QuizQuestion(image:"Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
     QuizQuestion(image:"Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)]
+
 private func convert(model: QuizQuestion) -> QuizStepViewModel {
     let questionStep = QuizStepViewModel(
         image: UIImage(named: model.image) ?? UIImage(),
@@ -31,91 +42,103 @@ private func convert(model: QuizQuestion) -> QuizStepViewModel {
     return questionStep
 }
 
+
 final class MovieQuizViewController: UIViewController {
-@IBOutlet private weak var questionLabel: UILabel!
-@IBOutlet private weak var questionNuberLabel: UILabel!
-@IBOutlet private weak var posterImageView: UIImageView!
+    @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private weak var questionNuberLabel: UILabel!
+    @IBOutlet private weak var posterImageView: UIImageView!
     
-override func viewDidLoad() {
-    super.viewDidLoad()
-    show(quiz: convert(model: currentQuestion))
-    
-    
-}
-private func show(quiz step: QuizStepViewModel) {
-    questionLabel.text = step.question
-    questionNuberLabel.text = step.questionNumber
-    posterImageView.image = step.image
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        show(quiz: convert(model: currentQuestion))
+        posterImageView.layer.cornerRadius = 20
+        posterImageView.layer.masksToBounds = true
+        
+    }
+    private func show(quiz result: QuizResultsViewModel) {
+        
+        let alert = UIAlertController(title: "Раунд завершён",
+                                      message: "Ваш результат \(correctAnswers)/10",
+                                      preferredStyle: .alert) // .alert или .actionSheet
+        
+        
+        let action = UIAlertAction(title: "Сыграть ещё раз", style: .default) { _ in
+            currentQuestionIndex = 0
+            correctAnswers = 0
+            
+            let firstQuestion = questions[currentQuestionIndex] // 2
+            let viewModel = convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    private func show(quiz step: QuizStepViewModel) {
+        questionLabel.text = step.question
+        questionNuberLabel.text = step.questionNumber
+        posterImageView.image = step.image
+    }
+    private func showAnswerResult(isCorrect: Bool) {
+        if isCorrect == true {
+            posterImageView.layer.borderColor = UIColor.ypGreen.cgColor
+            correctAnswers += 1
+        } else {
+            posterImageView.layer.borderColor = UIColor.ypRed.cgColor
+        }
+        
+        posterImageView.layer.borderWidth = 6
+        posterImageView.layer.cornerRadius = 20
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.showNextQuestionOrResults()
+        }
+    }
+    private func showNextQuestionOrResults() {
+        if currentQuestionIndex == questions.count - 1 {
+            let text = "Ваш результат: \(correctAnswers)/10"
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: text,
+                buttonText: "Сыграть ещё раз")
+            show(quiz: viewModel)
+            posterImageView.layer.borderColor = UIColor.clear.cgColor
+        } else {
+            currentQuestionIndex += 1
+            posterImageView.layer.borderColor = UIColor.clear.cgColor
+            
+            let nextQuestion = questions[currentQuestionIndex]
+            let viewModel = convert(model: nextQuestion)
+            show(quiz: viewModel)
+            
+        }
     }
     
-@IBAction private func yesButtonClick(_ sender: Any) {
-}
-@IBAction private func noButtonClick(_ sender: Any) {
-}
     
+    @IBAction private func yesButtonClick(_ sender: Any) {
+        
+        (sender as? UIButton)?.isEnabled = false
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = true
+        
+        
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            (sender as? UIButton)?.isEnabled = true
+        }
+    }
+    @IBAction private func noButtonClick(_ sender: Any) {
+        (sender as? UIButton)?.isEnabled = false
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
+        
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            (sender as? UIButton)?.isEnabled = true
+        }
+    }
 }
 
-
-/*
-Mock-данные
-
-
-Картинка: The Godfather
-Настоящий рейтинг: 9,2
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: ДА
-
-
-Картинка: The Dark Knight
-Настоящий рейтинг: 9
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: ДА
-
-
-Картинка: Kill Bill
-Настоящий рейтинг: 8,1
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: ДА
-
-
-Картинка: The Avengers
-Настоящий рейтинг: 8
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: ДА
-
-
-Картинка: Deadpool
-Настоящий рейтинг: 8
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: ДА
-
-
-Картинка: The Green Knight
-Настоящий рейтинг: 6,6
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: ДА
-
-
-Картинка: Old
-Настоящий рейтинг: 5,8
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: НЕТ
-
-
-Картинка: The Ice Age Adventures of Buck Wild
-Настоящий рейтинг: 4,3
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: НЕТ
-
-
-Картинка: Tesla
-Настоящий рейтинг: 5,1
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: НЕТ
-
-
-Картинка: Vivarium
-Настоящий рейтинг: 5,8
-Вопрос: Рейтинг этого фильма больше чем 6?
-Ответ: НЕТ
-*/
