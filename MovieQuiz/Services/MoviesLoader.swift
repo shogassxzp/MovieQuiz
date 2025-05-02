24e280d9b57ad7b8bc1cf3986c1e944ca299a72c
rssxzp <rssxzp@icloud.com>
2025-05-03 02:52:33 +0500

    complete sprint_06: Добавлены работа с сетью и разнообразие вопросов

---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,42 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Игнат Рогачевич on 2.05.25.
+//
+
+import Foundation
+
+protocol MoviesLoading {
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void)
+}
+
+struct MoviesLoader: MoviesLoading {
+    private let networkClient = NetworkClient()
+    private var mostPopularMoviesUrl: URL {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf")
+        else {
+            preconditionFailure("Unable to construct mostPopularMovies")
+        }
+        return url
+    }
+    
+    func loadMovies(handler: @escaping (Result<MostPopularMovies, Error>) -> Void) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let movies = try decoder.decode(MostPopularMovies.self, from: data)
+                    handler(.success(movies))
+                } catch {
+                    handler(.failure(error))
+                }
+                
+            case .failure(let error):
+                handler(.failure(error))
+            }
+        }
+        
+    }
+}
